9eb5ea3b6f1d4c162d0167a90420741e5aac74fb
k100ed10-alt <k100ed10@gmail.com>
2026-09-12 11:32:54 +0400

    Use Hasad Firebase project for teacher secret

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDK0zMEtDwBVzWk8QhMWmheO0qTOFrH7ts",
+  authDomain: "payh-2b37e.firebaseapp.com",
+  projectId: "payh-2b37e",
+  storageBucket: "payh-2b37e.firebasestorage.app",
+  messagingSenderId: "384010113582",
+  appId: "1:384010113582:web:0f8cfa619a71bbec8305bf"
 };
 
 window.FALLBACK_TEACHER_CODE = "123456";
